d658142043ae93a34b91ce442ba558c638baf447
douglas <douglas@vixonline.com.br>
2019-05-21 21:24:19 -0300

    dfg

--- js/function.js
+++ js/function.js
@@ -1,22 +1,22 @@
 $(document).ready(function() {
-  $(".animsition").animsition({
-    inClass               :   'fade-in',
-    outClass              :   'fade-out-down',
-    inDuration            :    800,
-    outDuration           :    800,
-    linkElement           :   '.animsition-link',
-    // e.g. linkElement   :   'a:not([target="_blank"]):not([href^=#])'
-    loading               :    true,
-    loadingParentElement  :   'body',
-    loadingClass          :   'animsition-loading',
-    unSupportCss          : [ 'animation-duration',
-                              '-webkit-animation-duration',
-                              '-o-animation-duration'
-                            ],
-    
-    overlay               :   false,    
-    overlayClass          :   'animsition-overlay-slide',
-    overlayParentElement  :   'body'
-  });
+    let direcao = window.location.pathname == '/' ? 'left' : 'right';
+    console.log('Direção: '+direcao+' pat: '+window.location.pathname);
+    $(".animsition").animsition({
+        inClass               : 'fade-in-'+direcao,
+        outClass              : 'fade-out-'+direcao,
+        inDuration            : 800,
+        outDuration           : 800,
+        linkElement           :   'a:not([target="_blank"]):not([href^=#])',
+        loading               :    true,
+        loadingParentElement  :   'body',
+        loadingClass          :   'animsition-loading',
+        unSupportCss          : [ 'animation-duration',
+            '-webkit-animation-duration',
+            '-o-animation-duration'
+        ],
+        overlay               :   false,    
+        overlayClass          :   'animsition-overlay-slide',
+        overlayParentElement  :   'body'
+    });
 });
 
