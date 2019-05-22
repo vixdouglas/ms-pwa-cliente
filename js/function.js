b804de7e14423b5778173c7a3415d2be645172d9
douglas <douglas@vixonline.com.br>
2019-05-21 21:34:52 -0300

    fg

--- js/function.js
+++ js/function.js
@@ -2,18 +2,15 @@ $(document).ready(function() {
     let direcao = window.location.pathname.replace("/ms-pwa-cliente", "") == '/' ? 'left' : 'right';
     console.log('Direção: '+direcao+' pat: '+window.location.pathname);
     $(".animsition").animsition({
-        inClass               : 'fade-in-'+direcao,
-        outClass              : 'fade-out-'+direcao,
-        inDuration            : 800,
-        outDuration           : 800,
+        inClass               :   'fade-in-'+direcao,
+        outClass              :   'fade-out-'+direcao,
+        inDuration            :   800,
+        outDuration           :   800,
         linkElement           :   'a:not([target="_blank"]):not([href^=#])',
         loading               :    true,
         loadingParentElement  :   'body',
         loadingClass          :   'animsition-loading',
-        unSupportCss          : [ 'animation-duration',
-            '-webkit-animation-duration',
-            '-o-animation-duration'
-        ],
+        unSupportCss          :   ['animation-duration', '-webkit-animation-duration', '-o-animation-duration'],
         overlay               :   false,    
         overlayClass          :   'animsition-overlay-slide',
         overlayParentElement  :   'body'
